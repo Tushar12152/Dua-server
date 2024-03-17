b49744f019be026d5bf6e807b39e91159cb8c89f
Tushar12152 <tusharimran470@gmail.com>
2024-03-17 10:26:12 +0600

    single data get

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ async function run() {
          const result=await DuaCollections.find().toArray()
          res.send(result)
     })
+    app.get('/dua/:id',async(req,res)=>{
+         const id=req.params.id
+         const result=await DuaCollections.findOne({_id:new ObjectId(id)})
+         res.send(result)
+    })
 
 
 
